5378a9bdbcff6feeca9ccd1b11c28689d8fb150c
Patrick Duvall <duvallp208@gmail.com>
2019-09-05 09:54:38 -0600

    Get object to modify title Dom, still bad formatting

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,10 +1,22 @@
 import { expect } from "chai"
 import sinon from "sinon"
+var sw = require('selenium-webdriver');
+var driver = new sw.Builder()
+  .withCapabilities(sw.Capabilities.chrome())
+  .build()
+
+// And then...
+var chai = require('chai');
+var chaiWebdriver = require('chai-webdriver');
+chai.use(chaiWebdriver(driver));
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("top word", () => {
+    it("displays the top word and its word count", () => {
+      driver.get('/');
+      chai.expect('#site-container section.word-container').dom.to.contain.text("Top Word!");
     })
   })
 })
 
+// When the user comes to the page, they should see the top word and it's count on the right side of the page.
